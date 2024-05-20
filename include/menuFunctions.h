6d2c3b2537aa877b5d3a28272caf84583e6956d4
OgrodowskiJedrzej <jedrekogro@gmail.com>
2024-05-20 16:29:08 +0200

    Added function to read from file, some error handling

--- include/menuFunctions.h
+++ include/menuFunctions.h
@@ -2,3 +2,4 @@
 #include <vector>
 
 void generateInputValues(int count, int maxValue, int maxWeight, std::vector<int> &values, std::vector<int> &weights);
+int readFromFile(const std::string filename, std::vector<int> &values, std::vector<int> &weights);
--- source/menuFunctions.cpp
+++ source/menuFunctions.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 #include <random>
 
 void generateInputValues(int count, int maxValue, int maxWeight, std::vector<int> &values, std::vector<int> &weights)
@@ -17,4 +19,48 @@ void generateInputValues(int count, int maxValue, int maxWeight, std::vector<int
         values.push_back(pi);
         weights.push_back(wi);
     }
+}
+
+int readFromFile(const std::string filename, std::vector<int> &values, std::vector<int> &weights)
+{
+    std::ifstream inputFile(filename);
+
+    if (!inputFile)
+    {
+        std::cerr << "Error file opening!" << std::endl;
+        return 1;
+    }
+    std::string line;
+    if (!getline(inputFile, line))
+    {
+        std::cerr << "Error no capacity value!" << std::endl;
+        return 1;
+    }
+    int c = 0;
+    std::istringstream issC(line);
+    if (!(issC >> c))
+    {
+        std::cerr << "Error with reading capacity" << std::endl;
+        return 1;
+    }
+    int i;
+    if (!(inputFile >> i))
+    {
+        std::cerr << "Error no count value!" << std::endl;
+        return 1;
+    }
+    for (int j = 0; j < i; ++j)
+    {
+        int pi, wi;
+        if (!(inputFile >> pi >> wi))
+        {
+            std::cerr << "Error not enough of pi wi values!" << std::endl;
+            return 1;
+        }
+        values.push_back(pi);
+        weights.push_back(wi);
+    }
+
+    inputFile.close();
+    return c;
 }
